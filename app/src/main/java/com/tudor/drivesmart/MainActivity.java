557b28e05f745700f81365d0dd8fb15b61a01f45
Tudor <branettudor22@gmail.com>
2024-05-02 16:24:27 +0300

    Forgot password activity done

--- app/src/main/java/com/tudor/drivesmart/MainActivity.java
+++ app/src/main/java/com/tudor/drivesmart/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), SignupActivity.class);
+                Intent i = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
                 startActivity(i);
             }
         });
